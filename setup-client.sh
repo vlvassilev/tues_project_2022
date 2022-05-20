d0f8e4c4c71187fb1db99537488ef5646f7bb0e3
Vladimir Vassilev <vladimir@lightside-instruments.com>
2022-05-20 14:13:14 +0200

    Starting with cd to have correct repository paths

--- setup-client.sh
+++ setup-client.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 #Install tntapi client side scripting
-
-sudo apt-get -y install  python3-distutils python3 python3-lxml libxml2-utils python3-paramiko
+cd
+apt-get -y install  python3-distutils python3 python3-lxml libxml2-utils python3-paramiko
 git clone https://github.com/vlvassilev/litenc.git
 cd litenc
 python setup.py install
